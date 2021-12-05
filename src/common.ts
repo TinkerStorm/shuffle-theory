77afe33700f9296ea942f78aa24d97e16fe9be5a
Junior <sudojunior@outlook.com>
2021-12-05 03:04:08 +0000

    `scale-to` import override, ext module failure * `scale-to` is not able to run since there is no index file - the dist was never included with the release...

--- src/common.ts
+++ src/common.ts
@@ -72,3 +72,14 @@ export function scaleTo(total: number, list: number[]) {
 
   return list.map(el => el * scale);
 }
+
+export function scaleObjectTo(total: number, object: { [key: string]: number }): { [key: string]: number } {
+  object = { ...object }; // ensure a clone is made to prevent mutation
+  const sum = Object.values(object).reduce((a, b) => a + b, 0);
+  const ratio = total / sum;
+
+  return Object.keys(object).reduce((acc, key) => {
+    acc[key] = object[key] * ratio;
+    return acc;
+  }, {} as { [key: string]: number });
+}
